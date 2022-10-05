7a80724327cca4d368ba66c6ee4514a81b491e24
N Praveen Kumar <praveen@Ns-MacBook-Air.local>
2022-10-05 17:26:43 +0530

    Fixed Design for Admin

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -3,10 +3,8 @@ import Teachers from "./admin/Teachers";
 
 import { ReactComponent as SunIcon } from "../assets/sun.svg";
 import { ReactComponent as SearchIcon } from "../assets/Search.svg";
-import { ReactComponent as ProfileIcon } from "../assets/Profile.svg";
 import { ReactComponent as SettingIcon } from "./icon.svg";
 import { ReactComponent as LogoutIcon } from "../assets/Logout.svg";
-import { ReactComponent as NotificationIcon } from "../assets/Notification-1.svg";
 import LordIcon from "../components/UI/lordIcons/LordIcon";
 import "../styles/Admin.scss";
 
